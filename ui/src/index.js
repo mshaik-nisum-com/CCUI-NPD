720475c196cd8b9902c8a5229f92c4f9ddf2e7bb
Vanavaraju <Vanava.Raju.Adabala@macys.com>
2018-10-05 14:47:51 +0530

    CCUI-002: Added folder structure for redux flow.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { BrowserRouter } from 'react-router-dom'
+import logger from 'redux-logger'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)))
+
+ReactDOM.render(
+<Provider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</Provider>
+, document.getElementById('root'));
 serviceWorker.unregister();
