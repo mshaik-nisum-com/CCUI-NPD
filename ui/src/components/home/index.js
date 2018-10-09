499c77e11c994550c41d54b99071b9518729a045
Rajesh Sagar <rammula@nisum.com>
2018-10-09 12:36:36 +0530

    CCUI:14 components are ready to integrate with api calls

--- ui/src/components/home/index.js
+++ ui/src/components/home/index.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Header from '../header'
 import Sidebar from '../sidebar'
 import Mainbody from '../mainbody'
-
+import BrandNav from '../brandNav'
+import '../../css/home.css'
 class Homepage extends Component {
     state = {  }
     render() { 
         return ( 
             <div>
                 <Header />
+                <BrandNav/>
                 <div id="wrapper">
                     <Sidebar />
                     <div id="content-wrapper">
