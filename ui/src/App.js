1b378acaf03c117237a350ffabb9a8c0b692f86f
nlavania-nisum-com <nitin.lavania@macys.com>
2018-10-09 19:13:52 +0530

    created login component

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import Login from './components/login/Login-auth';
-import Homepage from './components/home';
 import './css/common.css';
 import '../src/css/bootstrap-cdn.css'
+import AuthContainer from './components/login/AuthContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
+        <AuthContainer />
         {/* Enable the below component to view the home page
         <Homepage /> */}
       </div>
