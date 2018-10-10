82ec38ec0c1a9d1bd462e5764b0d0529515a38cc
vjchaitanya <vjchaitanya@gmail.com>
2018-10-10 16:32:04 +0530

    changes for excluding api from token authorization

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -1,30 +1,29 @@
-var jwt = require('jsonwebtoken')
+var jwt = require("jsonwebtoken");
 
-
-module.exports = function (request, response, next) {
-
-    if(!(request.path ===  '/auth/login' || request.path === '/auth/register' || request.path === '/auth/logout')){
-        var token = request.get('Authorization').split(' ')[1];
-        try{
-            if(request.session.isExist === true){
-            var decoded = jwt.verify(token, process.env.CRYPTR_KEY);
-            next();
-            }else{
-                response.status(400).json({
-                    errMsg : 'Invalid Token'
-                })
-            }
-        }catch (e) {
-            response.status(400).json({
-                errMsg : 'Invalid Request'
-            })
-        }
-    }else if(request.path === '/auth/logout'){
-        delete(request.session.isExist);
+module.exports = function(request, response, next) {
+  if (!(request.path === "/auth/login" ||
+      request.path === "/auth/register" ||
+      request.path === "/auth/logout" ||
+      request.path === "/markets/marketList")) {
+    var token = request.get("Authorization").split(" ")[1];
+    try {
+      if (request.session.isExist === true) {
+        var decoded = jwt.verify(token, process.env.CRYPTR_KEY);
         next();
-
-    }else {
-        next();
+      } else {
+        response.status(400).json({
+          errMsg: "Invalid Token"
+        });
+      }
+    } catch (e) {
+      response.status(400).json({
+        errMsg: "Invalid Request"
+      });
     }
-
-}
+  } else if (request.path === "/auth/logout") {
+    delete request.session.isExist;
+    next();
+  } else {
+    next();
+  }
+};
