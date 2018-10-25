ad80e346f7c94b0797349fe0a2cd6e65c70d162f
Hari Krishna V <Harikrishna.Vanapamula@macys.com>
2018-10-25 10:36:37 +0530

    Added Market Service

---
+++ api/service/marketService.js
@@ -0,0 +1,13 @@
+var marketDao = require("../dao/marketDao");
+
+
+module.exports = {
+  marketList: (req, res) => {
+    marketDao.marketList(req, res);
+  },
+  fetchBrands: (req, res) => {
+    marketDao.fetchBrands(req, res);
+  }
+};
+
+
