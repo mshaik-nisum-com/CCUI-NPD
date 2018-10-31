1320e613bb26a80aa19e4336e796f70ee1bf5e84
Rajesh Sagar <rammula@nisum.com>
2018-10-31 16:25:25 +0530

    change of home icon

--- ui/src/components/brandNav/index.js
+++ ui/src/components/brandNav/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import homeImage from '../../images/home.png';
 
 export default class BrandNav extends Component {
   render() {
     return (
       <div>
             <nav className="navbar navbar-expand navbar-dark  static-top navHeader">
-               <img className="home-Icon" src="https://png.icons8.com/metro/50/000000/home.png"/>
+               <img className="home-Icon" src={homeImage}/>
                <div className="order-details" ><b>Brand</b>:  {this.props.orderDetails.brandName}</div>
                <div className="order-details" ><b>orderNumber</b>: {this.props.orderDetails.orderNumber}</div>
                <div className="order-details" ><b>orderDate</b>  : {this.props.orderDetails.createdDate}</div>
